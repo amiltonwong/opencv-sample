fc93f5eaee2b6eef0ddbc50f21224290353ea2c5
chilijung <chilijung@gmail.com>
2013-11-13 13:44:01 +0800

    add corner detector

---
+++ corner_detector/build/CMakeFiles/corner_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/corner_detector.dir/corner_detector.cpp.o"
+  "corner_detector.pdb"
+  "corner_detector"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/corner_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- mat_test/mat_test.cpp
+++ mat_test/mat_test.cpp
@@ -6,7 +6,7 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat M(2,2, CV_8UC3, Scalar(0,0,255));
-    	cout << "M = " << endl << " " << M << endl << endl;
+  Mat M(2,2, CV_8UC3, Scalar(0,0,255));
+      cout << "M = " << endl << " " << M << endl << endl;
 
 }
